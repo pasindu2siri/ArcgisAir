f7b33ec64c23dc95439e06d0ce142b50eb46af7d
pasindu2siri <pasindu2siri@gmail.com>
2021-02-06 01:24:10 -0800

    Changed Listview to CardView using RecyclerView. Also using the IQAir API now

---
+++ app/src/main/java/com/example/arcgisair/dataAccess/AQIInformation.java
@@ -0,0 +1,16 @@
+package com.example.arcgisair.dataAccess;
+
+public class AQIInformation {
+    public static String[] cityArray = {"Pasadena", "Gardena"};
+
+    public static int[] AQIArray = {};
+
+    public static double[] longitudeArray = {};
+
+    public static double[] latitudeArray = {};
+
+    public static int[] id = {2};
+
+
+
+}
