6c6946545333af4ab0bf2e05b1cec7c6a7dd2cca
pasindu2siri <pasindu2siri@gmail.com>
2021-03-06 14:16:27 -0800

    Added complete Firebase Functionailty

--- app/src/main/java/com/example/arcgisair/MainActivity.java
+++ app/src/main/java/com/example/arcgisair/MainActivity.java
@@ -1,32 +1,39 @@
 package com.example.arcgisair;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView navView;
-    private FirebaseFirestore db;
-    private FirebaseAuth firebaseAuth;
 
 
 
@@ -37,16 +44,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-        DocumentReference userDocument =  firebaseFirestore.collection("Users").document(user.getUid());
-        DocumentReference citiesDocument =  firebaseFirestore.collection("Cities").document(user.getUid());
+        FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+        DocumentReference docIdRef = rootRef.collection("Users").document(user.getUid());
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        System.out.print("User: " + user.getUid() + " exists");
+                    } else {
+                        Map<String, Object> map = new HashMap<>();
+                        List<String> Cities = new ArrayList<String>();
+                        map.put("Cities", Cities);
+                        rootRef.collection("Users").document(user.getUid()).set(map);
+                    }
+                }
+            }
+        });
 
-        Map< String, Object > User = new HashMap< >();
-        User.put("Uid", user.getUid());
 
 
-        userDocument.set(User);
-        citiesDocument.set(User);
 
 
 
